f658c29694d0839ddc0474c81136e79f317bb5ef
jorgemagnus <jorgemagnus_rn@hotmail.com>
2022-10-04 17:12:08 -0300

    Atualizando projeto desafio 01

--- POTTENCIAL/Projetos/ExemploFundamentos/ExemploFundamentos/Program.cs
+++ POTTENCIAL/Projetos/ExemploFundamentos/ExemploFundamentos/Program.cs
@@ -1,27 +1,71 @@
-﻿// Para usar as classes  que estão no Common.Models
-using ExemploFundamentos.Common.Models; 
+﻿using System;
 
-//Exemplo de comentários de uma linha.
+class DIO{
 
-// Instância da classe pessoa
-Pessoa p = new Pessoa();
+static void Main(string[] args)
+{
+     
+        Console.WriteLine("Informe Opção 1 = 234 ou Opção 2 = 4421");
+
+        int numeroInformado = 0, 
+        opcaoSelecionada = 0;
+
+        opcaoSelecionada = Convert.ToInt32(Console.ReadLine());
+
+        if (opcaoSelecionada == 1)
+         {
+             numeroInformado = 234;
+         }
+         else if (opcaoSelecionada == 2)
+        {
+             numeroInformado = 4421;
+         }
+         else
+         {
+             Console.WriteLine("Opção inválida!");            
+         }  
+       
+        
+        List<int> ListaNumeros = new List<int>();
+        while (numeroInformado > 0)
+        {
+            
+            ListaNumeros.Add(numeroInformado % 10);
+            numeroInformado = numeroInformado / 10;
+            
+        }
+
+        ListaNumeros.Reverse();
+
+         int contadorForeach = 0;
+        foreach(int numLista in ListaNumeros)
+         {
+          Console.WriteLine($"Posição Nº {contadorForeach} - {numLista}");
+            contadorForeach++;
+         }
+
+        int p = 0, s = 0;
 
 
-/*
-  Fazendo comentários em varias linhas(texto grande.)
-  Atribuir o nome e idade para pessoa
-  passando o nome jorge
-  passando a idade 47
-  e pode escrever várias linhas aqui
-  linha ....  
-*/
+        if (ListaNumeros.Count == 3)
+        {
+            p = ListaNumeros[0] * ListaNumeros[1] * ListaNumeros[2];
+            s = ListaNumeros[0] + ListaNumeros[1] + ListaNumeros[2];
+            Console.WriteLine(Convert.ToInt32(p - s));
+          
+        } else if (ListaNumeros.Count == 4)
+        {
+           p = ListaNumeros[0] * ListaNumeros[1] * ListaNumeros[2] * ListaNumeros[3];
+           s = ListaNumeros[0] + ListaNumeros[1] + ListaNumeros[2] + ListaNumeros[3];
+            Console.WriteLine(Convert.ToInt32(p - s));            
+        }else
+        {
+            Console.WriteLine("Não foi informado um valor válido!");
+        }
 
-p.Nome = "Jorge";
-p.Idade = 47;
+               
+    }
 
-// Faz a pessoa se apresentar.
-p.Apresentar();
+}
 
-Calculadora calc = new Calculadora();
-calc.Somar(3, 4);
-calc.Subtrair(3, 5);
+
