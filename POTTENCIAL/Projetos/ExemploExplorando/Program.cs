b2173ef0f5c6da898767734e9ab1426adc23f5c5
jorgemagnus <jorgemagnus_rn@hotmail.com>
2022-10-05 21:41:44 -0300

    Adicionando projeto ExemploExplorando, dois arquivos docx

---
+++ POTTENCIAL/Projetos/ExemploExplorando/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExemploExplorando;
+
+Pessoa p1 = new Pessoa();
+Console.WriteLine("Digite se nome:");
+p1.Nome = Console.ReadLine();
+Console.WriteLine("Digite seu Sobrenome");
+p1.Sobrenome = Console.ReadLine();
+Console.WriteLine("Digite sua idade:");
+p1.Idade = Convert.ToInt32(Console.ReadLine());
+p1.Apresentar();
+
+
+
