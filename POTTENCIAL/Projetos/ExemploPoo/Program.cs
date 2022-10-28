667accc043edca7b19aacc86bb69aaea10b1ad1f
jorgemagnus <jorgemagnus_rn@hotmail.com>
2022-10-27 21:39:56 -0300

    Adicionando estudo POO, Abstração e Encapsulamento

---
+++ POTTENCIAL/Projetos/ExemploPoo/Program.cs
@@ -0,0 +1,18 @@
+﻿using ExemploPoo.Models;
+
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Jorge";
+p1.Idade = 47;
+p1.Apresentar();
+
+
+Console.WriteLine("----------------------");
+
+
+ContaCorrente c1 = new ContaCorrente(123,1000);
+c1.ExibirSaldo();
+c1.Sacar(500);
+c1.ExibirSaldo();
+
+
